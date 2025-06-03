df718c6a379d950dfa8c4696c33a483e70114ede
younaJ00 <you.na.joo0@gmail.com>
2025-06-03 15:02:28 +0900

    feat: hello service 구현 및 di 방식으로 controller 에 주입

--- src/main/java/com/example/myfirstspring/hello/HelloController.java
+++ src/main/java/com/example/myfirstspring/hello/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.myfirstspring.hello;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class HelloController {
+    private final HelloService helloService;
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return helloService.sayHello();
     }
 }
---
+++ src/main/java/com/example/myfirstspring/hello/HelloService.java
@@ -0,0 +1,11 @@
+package com.example.myfirstspring.hello;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class HelloService {
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
